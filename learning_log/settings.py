0d37d43f2b82b70443f138cd9d3ae09246b06ae6
graciedevine <gracie.devine@gmail.com>
2021-04-17 09:55:41 -0400

    Set DEBUG based o environment variables.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,3 +137,8 @@ LOGIN_REDIRECT_URL = 'learning_logs:index'
 # Heroku settings.
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
