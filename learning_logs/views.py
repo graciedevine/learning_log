97d1f5ff82fbd899fc5ce111f1cd81c1a1fc780c
graciedevine <gracie.devine@gmail.com>
2021-05-04 16:06:39 -0400

    Removed some packages from requirements.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -5,19 +5,20 @@ from django.http import Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
+
 def index(request):
     """The home page for Learning Log."""
-    return render(request, 'learning_logs/index.html')
+    return render(request, "learning_logs/index.html")
 
 
 @login_required
 def show_all_topics(request):
     """Show all topics."""
-    
-    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-    
-    context = {'topics': topics}
-    return render(request, 'learning_logs/show_all_topics.html', context)
+
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
+
+    context = {"topics": topics}
+    return render(request, "learning_logs/show_all_topics.html", context)
 
 
 @login_required
@@ -28,38 +29,38 @@ def topic(request, topic_id):
     if topic.owner != request.user:
         raise Http404
 
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "learning_logs/topic.html", context)
 
 
 @login_required
 def new_topic(request):
     """Add a new topic."""
-    if request.method == 'POST':
+    if request.method == "POST":
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
             new_topic = form.save(commit=False)
             new_topic.owner = request.user
             new_topic.save()
-            return redirect(F'/show_all_topics')
+            return redirect(f"/show_all_topics")
     else:
         # No data submitted; create a blank form.
         form = TopicForm()
 
     # Display a blank or invalid form.
-    context = {'form': form}
-    return render(request, 'learning_logs/new_topic.html', context)
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
+
 
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     # topic = Topic.objects.get(id=topic_id)
     topic = get_object_or_404(Topic, id=topic_id)
- 
 
-    if request.method != 'POST':
+    if request.method != "POST":
         # No data submitted; create a blank form.
         form = EntryForm()
     else:
@@ -70,11 +71,11 @@ def new_entry(request, topic_id):
             new_entry.topic = topic
             new_entry.owner = request.user
             new_entry.save()
-            return redirect('learning_logs:topic', topic_id=topic_id)
-    
+            return redirect("learning_logs:topic", topic_id=topic_id)
+
     # Display a blank or invalid form.
-    context = {'topic' : topic, 'form' : form}
-    return render(request, 'learning_logs/new_entry.html', context)
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
 
 
 @login_required
@@ -86,7 +87,7 @@ def edit_entry(request, entry_id):
     if topic.owner != request.user:
         raise Http404
 
-    if request.method != 'POST':
+    if request.method != "POST":
         # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
     else:
@@ -94,10 +95,10 @@ def edit_entry(request, entry_id):
         form = EntryForm(instance=entry, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('learning_logs:topic', topic_id=topic.id)
+            return redirect("learning_logs:topic", topic_id=topic.id)
 
-    context = {'entry': entry, 'topic': topic, 'form': form}
-    return render(request, 'learning_logs/edit_entry.html', context)
+    context = {"entry": entry, "topic": topic, "form": form}
+    return render(request, "learning_logs/edit_entry.html", context)
 
 
 @login_required
@@ -108,21 +109,11 @@ def delete_entry(request, entry_id):
 
     if topic.owner != request.user:
         raise Http404
-    
-    if request.method == 'POST':
-        if request.POST['confirm'] == 'yes':
+
+    if request.method == "POST":
+        if request.POST["confirm"] == "yes":
             entry.delete()
-        return redirect('learning_logs:topic', topic_id=topic.id)
-        
-    context = {'entry': entry, 'topic': topic}
-    return render(request, 'learning_logs/delete_entry.html', context)
+        return redirect("learning_logs:topic", topic_id=topic.id)
 
-    # if request.method == 'POST' and 'delete' in request.POST:
-    #     Topic.objects.filter(id=topic_id).delete()
-
-    
-
-
-
-
-
+    context = {"entry": entry, "topic": topic}
+    return render(request, "learning_logs/delete_entry.html", context)
